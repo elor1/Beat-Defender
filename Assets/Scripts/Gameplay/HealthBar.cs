aa0b0449de2967c8f518dc331862372ed90c5670
elor1 <57156027+elor1@users.noreply.github.com>
2021-01-11 02:56:57 +0000

    Added health bar
    Added a health bar to the screen during waves

---
+++ Assets/Scripts/Gameplay/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public static Slider _healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _healthSlider = GetComponent<Slider>();
+        _healthSlider.maxValue = GameManager._playerStartingHealth;
+        _healthSlider.value = GameManager._playerStartingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_healthSlider)
+        {
+            _healthSlider.value = GameManager._playerHealth;
+        }
+    }
+}
